453d249d1e385bfa59234bbf52260005a37a13ab
Ruegnevel <mickael.garberi@laposte.net>
2022-07-05 15:17:49 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo $BUILD_USER
-echo $BUILD_USER_FIRST_NAME
-echo $BUILD_USER_LAST_NAME
-echo $BUILD_USER_ID
-echo $BUILD_USER_GROUPS
-echo $BUILD_USER_EMAIL
+# echo $BUILD_USER
+# echo $BUILD_USER_FIRST_NAME
+# echo $BUILD_USER_LAST_NAME
+# echo $BUILD_USER_ID
+# echo $BUILD_USER_GROUPS
+# echo $BUILD_USER_EMAIL
+
+if [ $BUILD_USER_FIRST_NAME == "SCM" ]
+then
+    echo "auto"
+else
+    echo "manuel"
+fi
